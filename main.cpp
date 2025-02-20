8e06510eb9348d6ace9fe11c8bded10ec75226fb
erazinkov <egorrazinkov@gmail.com>
2025-02-20 15:52:30 +0300

    Small fix

--- main.cpp
+++ main.cpp
@@ -84,21 +84,102 @@ void process(const ADCMEmulateQuery &query)
 
 void processBi()
 {
+    std::vector<std::pair<u_int8_t, u_int8_t> > mapBi
+    {
+        {GAMMA, 0},
+        {GAMMA, 1},
+        {GAMMA, 2},
+        {GAMMA, 3},
+        {GAMMA, 4},
+        {GAMMA, 5},
+        {GAMMA, 6},
+        {GAMMA, 7},
+        {GAMMA, 8},
+        {GAMMA, 9},
+        {GAMMA, 10},
+        {GAMMA, 11},
+        {GAMMA, 12},
+        {GAMMA, 13},
+        {UNKNOWN, 111},
+        {ALPHA, 0},
+        {ALPHA, 1},
+        {ALPHA, 2},
+        {ALPHA, 3},
+        {ALPHA, 4},
+        {ALPHA, 5},
+        {ALPHA, 6},
+        {ALPHA, 7},
+        {ALPHA, 8},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+        {UNKNOWN, 111},
+    };
+
     const ChannelMap pre;
     const QString fileNameInput{"/home/egor/shares/tmp/proba_t2_thin_2"};
-    QString fileNameOutput{"test"};
+
     Decoder decoder(fileNameInput.toStdString(), pre);
     auto p{decoder.positionsOfCMAPHeaders()};
 
-    QList<long> qp{QVector<long>(p.begin(), p.end())};
-    if (!qp.empty())
+    QList<long> positions{QVector<long>(p.begin(), p.end())};
+
+    if (!positions.empty())
     {
+        auto begin{0};
+        auto size{30};
+        auto overlap{0};
         QFile inputFile(fileNameInput);
         if (!inputFile.open(QIODevice::ReadOnly)) {
             qInfo() << "Can't open input file " << fileNameInput;
             return;
         }
-        qp.push_back(inputFile.size());
+        positions.push_back(inputFile.size());
+        auto end{positions.size()};
+
+        QDataStream in(&inputFile);
+
+        struct SelectedPosition {
+            long position;
+            qsizetype size;
+        };
+
+        while (begin < end - size - overlap)
+        {
+            QString fileNameOutput{fileNameInput};
+            QString postFix = QString("_from_%1_to_%2").arg(begin).arg(begin + size - 1);
+            fileNameOutput.append(postFix);
+            QFile outputFile(fileNameOutput);
+            if (!outputFile.open(QIODevice::WriteOnly)) {
+                qInfo() << "Can't open output file" << fileNameOutput;
+                return;
+            }
+            QDataStream out(&outputFile);
+            QList<SelectedPosition> selectedPositions;
+            for (auto i{begin}; i < begin + size; ++i)
+            {
+                selectedPositions.push_back({positions[i], positions[i + 1] - positions[i]});
+            }
+
+            for (const auto& item : selectedPositions)
+            {
+                QByteArray ba(item.size, 0);
+                in.device()->seek(item.position);
+                auto rb = in.readRawData(ba.data(), static_cast<int>(ba.size()));
+                if (rb == ba.size())
+                {
+                    out.writeRawData(ba.data(), static_cast<int>(ba.size()));
+                }
+            }
+            qInfo() << "File:" << fileNameOutput << "From:" << begin << "To:" << begin + size - 1;
+            outputFile.close();
+            begin += size;
+            begin -= overlap;
+        }
 
 
 
